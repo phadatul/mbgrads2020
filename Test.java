715c257f8109514481d8bac04bcde0f6b2b2976e
Swarga Sarkar <swargasarkar@gmail.com>
2020-09-24 18:18:23 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ import com.practical.entity.FoodItems;
 /**
  * 
  * @author Swarga Sarkar
- * @purpose This class contains the main functionby
+ * @purpose This class contains the main method
  *
  */
 public class Test {
@@ -15,6 +15,7 @@ public class Test {
 		FoodItems fi2= new FoodItems(102,"Meat", 100, 70, "21/12/2019", "21/12/2020", "yes");
 		FoodItems fi3= new FoodItems(103,"Sweets", 100,60, "21/12/2019", "21/12/2020", "yes");
 		FoodItems fi4= new FoodItems(104,"Rice", 100, 40, "21/12/2019", "21/12/2020", "yes");
+		
 		
 	}
 }
