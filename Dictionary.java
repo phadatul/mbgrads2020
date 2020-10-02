cfbd6924fc673b2b9b6ecd90dd4db7509ae9fcf3
Swarga Sarkar <swargasarkar@gmail.com>
2020-10-02 16:10:31 +0530

    Add files via upload
    Model class for project Dictionary

---
+++ Dictionary.java
@@ -0,0 +1,47 @@
+package com.hsbc.model;
+
+public class Dictionary {
+	
+	private String word;
+	private String meaning;
+	private String antonym;
+	private String synonym;
+	
+	public Dictionary() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+
+	public String getAntonym() {
+		return antonym;
+	}
+
+	public void setAntonym(String antonym) {
+		this.antonym = antonym;
+	}
+
+	public String getSynonym() {
+		return synonym;
+	}
+
+	public void setSynonym(String synonym) {
+		this.synonym = synonym;
+	}
+	
+
+}
